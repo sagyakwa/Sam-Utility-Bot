5a2e8dcc9ae41371839aa17842ccaf9c802c817f
sagyakwa <samuelagyen@gmail.com>
2020-05-03 19:35:14 -0400

    refined logger refined StreamListener subclass

--- Bot.py
+++ Bot.py
@@ -1,68 +1,105 @@
 import os
+import sys
 
+import wget
 from tweepy import StreamListener, Stream, OAuthHandler, API
 
 import util
 
+__file__ = 'Bot.py'
+
 
 class Bot:
 	def __init__(self):
+		self.logger = Logger()
+		self.logger.log('Logging in...')
 		self.api_key = util.api_key
 		self.api_secret_key = util.api_secret_key
 		self.access_token = util.access_token
 		self.access_token_secret = util.access_token_secret
-		self.master_user = "2472010408"
 		self.auth = OAuthHandler(self.api_key, self.api_secret_key)
 		self.auth.set_access_token(self.access_token, self.access_token_secret)
 		self.api = API(self.auth)
-		self.stream_listener = Streamer()
+		self.stream_listener = Streamer(api=self.api)
 		self.streaming = Stream(auth=self.api.auth, listener=self.stream_listener)
 		self.tracked_word = ['samiambot']
+		self.logger.log('Login successful\n')
 
-	def listen(self):
+	def listen(self, follow=util.user_to_follow):
+		self.logger.log('Listening...')
 		# set stream to follow only me and listen for my tweets
-		self.streaming.filter(follow=[self.master_user], is_async=True, track=self.tracked_word)
+		self.streaming.filter(follow=[follow], is_async=True, track=self.tracked_word)
+
+	def tweet(self, tweet):
+		self.api.update_status(status=tweet)
+
+	def reply(self, reply, status_id):
+		self.api.update_status(in_reply_to_status_id=status_id, status=reply)
 
 
-class Log:
-	def __init__(self, text):
+class Logger:
+	def __init__(self):
 		# Set location for getting absolute path
-		self.location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 		self.log_name = "log.txt"
 
+	def log(self, text):
 		# See if log exists in same folder and set appropriate file access mode
-		if os.path.exists(os.path.join(self.location, 'log.txt')):
+		if os.path.exists(os.path.join(sys.path[0], 'log.txt')):
 			mode = 'a'
 		else:
 			mode = 'w'
 
 		# open and write to log file
-		with open(os.path.join(self.location, 'log.txt'), mode) as log_file:
+		with open(os.path.join(sys.path[0], 'log.txt'), mode) as log_file:
 			log_file.write(f'{text}\n')
 
 
 class Streamer(StreamListener):
+	def __init__(self, api=None):
+		super().__init__(api=api)
+		self.logger = Logger()
+		self.api = api
+
 	def on_status(self, status):
+		bot = Bot()
 		if hasattr(status, 'retweeted_status'):
 			pass
 		else:
-			print(status.text)
+			self.logger.log("Found reply with @samiambot")
+			original_tweet_id = status.in_reply_to_status_id_str
+			if original_tweet_id is not None:
+				self.logger.log("Found Media")
+				status_attributes = {
+					'status_text': status.text,
+					'status_id': status.id
+				}
+				return self.get_media(original_tweet_id)
+
+			else:
+				self.logger.log("No media found\n")
+				print("Not a reply")
+
+	def get_media(self, extended_tweet_id):
+		self.logger.log("Downloading media")
+		media_file = set()
+		original_tweet_media = self.api.statuses_lookup(extended_tweet_id, include_entities='media')
+		print(original_tweet_media)
+
+		for media in media_file:
+			wget.download(media, os.path.join(sys.path[0], 'media'))
+
+		self.logger.log("Media downloaded\n")
+		print("Media downloaded\n")
 
 	def on_error(self, status_code):
 		if status_code == 420:
-			Log(f'Status {status_code}')
+			self.logger.log(f'Status {status_code}')
 			return False
 		elif status_code == 429:
-			Log(f'Status {status_code}')
+			self.logger.log(f'Status {status_code}')
 			return False
 
 
-class Processor:
-	def __init__(self):
-		pass
-
-	def process_text(self, text):
-		print(list(text))
-
-
-Processor().process_text("Test")
+Bot().listen()
+# print(Processor().process_text("samiambot"))
+# Logger("Test")
