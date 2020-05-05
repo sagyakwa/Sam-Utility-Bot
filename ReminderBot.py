8ba5255ae8ec43a207b4fcf6a601ba01d81caeb9
sagyakwa <samuelagyen@gmail.com>
2020-05-05 18:31:45 -0400

    keep updated with everything

---
+++ ReminderBot.py
@@ -0,0 +1,14 @@
+import crontab as crontab
+from tweepy import OAuthHandler, API
+import configparser
+
+import util
+from Bot import Logger
+
+
+class Scheduler:
+	def __init__(self):
+		pass
+
+	def schedule(self, tweet_id, datetime):
+		pass
